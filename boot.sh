847d74b1b9589b44cb2c66c8825c0e5a56d25fa3
nodev918 <87605941+nodev918@users.noreply.github.com>
2022-09-25 15:47:09 +0800

    Create boot.sh

---
+++ boot.sh
@@ -0,0 +1,12 @@
+go mod init 
+go mod tidy
+touch main.go
+mkdir cmd
+mkdir configs
+mkdir internal
+mkdir internal/config
+mkdir internal/utils
+touch .gitignore
+touch .gitlab-ci.yml
+touch Dockerfile
+touch Makefile
