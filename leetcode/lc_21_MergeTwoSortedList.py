ddd84b095b0e831b7d2a096b518da0a231c651e8
nodev918 <nodev918@gmail.com>
2022-11-20 19:30:51 +0800

    update

--- leetcode/lc_21_MergeTwoSortedList.py
+++ leetcode/lc_21_MergeTwoSortedList.py
@@ -51,7 +51,9 @@ class Solution:
         while list1 and list2:               
             if list1.val < list2.val:
                 cur.next = list1
-                list1, cur = list1.next, list1
+
+                list1 = list1.next
+                cur = list1
             else:
                 cur.next = list2
                 list2, cur = list2.next, list2
