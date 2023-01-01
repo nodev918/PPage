c9f5742c7983cfb3431d5ab2ceae9058d7ad54fb
nodev918 <devnode77@gmail.com>
2023-01-01 21:47:49 +0800

    update autok3d.sh

--- autoscript/autok3d.sh
+++ autoscript/autok3d.sh
@@ -1,8 +1,8 @@
-# apt update
+#update apt
 sudo apt update
 
 # setup docker
-sudo apt install docker.io
+sudo apt install docker.io -y
 
 # setup k3d
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
@@ -10,4 +10,8 @@ curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 # setup kubectl
 sudo snap install kubectl --classic
 
+
+
+kubectl version
+k3d --version
 
