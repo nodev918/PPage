0e661a5ded58cbdd40fa66fc312cc0199f2d88e0
nodev918 <devnode77@gmail.com>
2023-01-01 21:24:35 +0800

    update autoscript

--- autoscript/autobasic.sh
+++ autoscript/autobasic.sh
@@ -3,6 +3,9 @@ SHIFTWIDTH="set shiftwidth=2"
 AUTOINDENT="set autoindent"
 TARGET=$(pwd)/.vimrc
 
+GIT_USER_NAME="nodev918"
+GIT_USER_EMAIL="devnode77@gmail.com"
+
 
 process(){
 	rm $TARGET
@@ -10,21 +13,28 @@ process(){
 	echo $TABSTOP >> $TARGET
 	echo $SHIFTWIDTH >> $TARGET
 	echo $AUTOINDENT >> $TARGET
-	echo "初始化完成, 開啟vim檢查有無正確設定"
 }
 
 cal(){
 	echo "hihi"
 }
 
-echo "初始化.vimrc, 要確定欸?(y/n)"
+echo "過程會初始化.vimrc, 確定的話輸入 y 按 enter"
 read user_determine
 
 if [ $user_determine == "y" ]
 then
 	process
+	echo "初始化完成, 開啟vim檢查有無正確設定"
 else
 	echo "好ㄅ,保留原始.vimrc"
 fi
+
+sudo apt update
+sudo apt install make -y
+
+git config --global user.name $GIT_USER_NAME
+git config --global user.email $GIT_USER_EMAIL
+
 
 
